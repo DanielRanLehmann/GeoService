cb3296620d8a32c61960a66707212107d83fe29d
Daniel Ran Lehmann <danielran11@gmail.com>
2017-02-24 16:48:00 +0100

    Initial Commit

---
+++ GeoService/GeoService.h
@@ -0,0 +1,13 @@
+//
+//  GeoService.h
+//  GeoService
+//
+//  Created by Daniel Ran Lehmann on 2/24/17.
+//  Copyright © 2017 Daniel Ran Lehmann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GeoService : NSObject
+
+@end
---
+++ GeoService/GeoService.m
@@ -0,0 +1,13 @@
+//
+//  GeoService.m
+//  GeoService
+//
+//  Created by Daniel Ran Lehmann on 2/24/17.
+//  Copyright © 2017 Daniel Ran Lehmann. All rights reserved.
+//
+
+#import "GeoService.h"
+
+@implementation GeoService
+
+@end
